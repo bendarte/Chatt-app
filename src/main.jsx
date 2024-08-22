ed5bfabdc4f1d07a75591366bdc6ec2b0c382351
bendarte <osman.stockholm@gmail.com>
2024-08-22 11:04:30 +0200

    lade till router-dome

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
 import App from './App';
 import './index.css';
 
